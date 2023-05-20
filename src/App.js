69aa717509ae4667e9d6ffc41b0e7b4dfba103df
Pranav <pranavborkar1999@gmail.com>
2023-05-20 21:12:52 +0530

    add button and css

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='div1'>
+    <h1>This Is My Third commit </h1>
+    <p>Hello if you want more information so click below button</p>
+    <button>click here</button>
     </div>
   );
 }
