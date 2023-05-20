9379fe717f27cdfac43be75f8de713550746b9d2
Pranav <pranavborkar1999@gmail.com>
2023-05-20 21:38:59 +0530

    fifth commit adding form

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
     <button>click here</button>
     <form>
       <span>Registration Form</span>
+      <br></br>
+      <label>First Name</label><br></br>
+      <input type='text' name='Pranav' placeholder='Enter First Name'></input>
+      <br></br>
+      <label>Last Name</label><br></br>
+      <input type='text' name='Borkar' placeholder='Enter Last Name'></input>
     </form>
     </div>
     
